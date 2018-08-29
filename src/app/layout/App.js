589c4ae78b6059bc38aa1eb54ae0391c4b929d12
tykowale <tykowale@gmail.com>
2018-08-28 20:25:08 -0500

    navbar and css

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
-        <EventDashboard/>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
       </div>
     );
   }
