ee051cfc624d4fbc2d4e23b07b7a6a1525f6051e
tykowale <tykowale@gmail.com>
2018-09-10 19:38:49 -0500

    start routing

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
 import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/navBar/NavBar";
+import EventDetailedPage from "../../features/event/eventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/peopleDashboard/PeopleDashboard";
+import UserDetailedPage from "../../features/user/userDetailed/UserDetailedPage";
+import SettingsDashboard from "../../features/user/settings/SettingsDashboard";
+import EventForm from "../../features/event/eventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
 
 class App extends Component {
   render() {
@@ -9,7 +16,13 @@ class App extends Component {
       <div>
         <NavBar/>
         <Container className="main">
-          <EventDashboard/>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/events" component={EventDashboard}/>
+          <Route path="/event/:id" component={EventDetailedPage}/>
+          <Route path="/people" component={PeopleDashboard}/>
+          <Route path="/profile/:id" component={UserDetailedPage}/>
+          <Route path="/settings" component={SettingsDashboard}/>
+          <Route path="/createEvent" component={EventForm}/>
         </Container>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./app/layout/App";
 import registerServiceWorker from "./registerServiceWorker";
@@ -8,7 +9,11 @@ import registerServiceWorker from "./registerServiceWorker";
 const rootEl = document.getElementById("root");
 
 function render() {
-  ReactDOM.render(<App/>, rootEl);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>,
+    rootEl);
 }
 
 if (module.hot) {
