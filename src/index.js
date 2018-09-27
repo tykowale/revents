595a2e4412d3e5d9f9d24bbec3bde6052cdfae5f
tykowale <tykowale@gmail.com>
2018-09-26 19:50:09 -0500

    scroll to top

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 import App from "./app/layout/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/common/util/ScrollToTop";
 
 const rootEl = document.getElementById("root");
 const store = configureStore();
@@ -15,7 +16,9 @@ function render() {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App/>
+        <ScrollToTop>
+          <App/>
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
     rootEl
