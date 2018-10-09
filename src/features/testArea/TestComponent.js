12e4afe08ae6ef0620127860a50d14adaf2cd222
tykowale <tykowale@gmail.com>
2018-10-08 19:52:29 -0500

    google maps api

--- src/features/testArea/TestComponent.js
+++ src/features/testArea/TestComponent.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Button } from "semantic-ui-react";
+import Script from "react-load-script";
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 import { incrementCounter, decrementCounter } from "./testActions";
+import config from "../../config";
 
 const mapState = (state) => ({
   data: state.test.data
@@ -13,14 +16,54 @@ const actions = {
 };
 
 class TestComponent extends Component {
+  state = {
+    address: "",
+    scriptLoaded: false
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    geocodeByAddress(this.state.address)
+    .then(results => getLatLng(results[0]))
+    .then(latLng => console.log("Success", latLng))
+    .catch(error => console.error("Error", error));
+  };
+
+  onChange = (address) => {
+    this.setState({ address });
+  };
+
+  handleScriptLoad = () => {
+    this.setState({
+      scriptLoaded: true
+    });
+  };
+
   render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange
+    };
     const { incrementCounter, decrementCounter, data } = this.props;
+
     return (
       <div>
+        <Script
+          url={`https://maps.googleapis.com/maps/api/js?key=${config.googleApiKey}&libraries=places`}
+          onLoad={this.handleScriptLoad}
+        />
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color="green" content="Increment"/>
         <Button onClick={decrementCounter} color="red" content="Decrement"/>
+        <br/>
+        <br/>
+        <form onSubmit={this.handleFormSubmit}>
+          {this.state.scriptLoaded &&
+          <PlacesAutocomplete inputProps={inputProps}/>}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
