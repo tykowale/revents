7377607c86661ff72d6e81a6aae3b63f3e310086
tykowale <tykowale@gmail.com>
2018-10-09 19:40:49 -0500

    place input on form

--- src/features/event/eventForm/EventForm.js
+++ src/features/event/eventForm/EventForm.js
@@ -10,6 +10,7 @@ import TextInput from "../../../app/common/form/TextInput";
 import TextArea from "../../../app/common/form/TextArea";
 import SelectInput from "../../../app/common/form/SelectInput";
 import DateInput from "../../../app/common/form/DateInput";
+import PlaceInput from "../../../app/common/form/PlaceInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -88,8 +89,20 @@ class EventForm extends Component {
                      placeholder="Tell us about your event"/>
 
               <Header sub color="teal" content="Event Location Details"/>
-              <Field name="city" type="text" component={TextInput} placeholder="Event City"/>
-              <Field name="venue" type="text" component={TextInput} placeholder="Event Venue"/>
+              <Field
+                name="city"
+                type="text"
+                component={PlaceInput}
+                options={{ types: ["(cities)"] }}
+                placeholder="Event City"
+              />
+              <Field
+                name="venue"
+                type="text"
+                component={PlaceInput}
+                options={{ types: ["establishment"] }}
+                placeholder="Event Venue"
+              />
               <Field
                 name="date"
                 type="text"
