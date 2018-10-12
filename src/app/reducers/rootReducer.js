631ed668c7cd30bcadd3d8653d0ed72a0fb39160
tykowale <tykowale@gmail.com>
2018-10-12 16:56:06 -0500

    auth reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { reducer as FormReducer } from "redux-form";
 import testReducer from "../../features/testArea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
 import modalReducer from "../../features/modal/modalReducer";
+import authReducer from "../../features/auth/authReducer";
 
 const rootReducer = combineReducers({
   form: FormReducer,
   test: testReducer,
   events: eventReducer,
-  modals: modalReducer
+  modals: modalReducer,
+  auth: authReducer
 });
 
 export default rootReducer;
