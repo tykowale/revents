075c79695feada03b769602acf30f79b3588540e
tykowale <tykowale@gmail.com>
2018-10-14 20:39:13 -0500

    increment and decrement async test

--- src/features/testArea/TestComponent.js
+++ src/features/testArea/TestComponent.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Button } from "semantic-ui-react";
-import Script from "react-load-script";
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 import { incrementCounter, decrementCounter } from "./testActions";
-import config from "../../config";
 import { openModal } from "../modal/modalActions";
 
 const mapState = (state) => ({
@@ -19,51 +16,16 @@ const actions = {
 
 class TestComponent extends Component {
 
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-
   state = {
     address: "",
     scriptLoaded: false
   };
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-
-    geocodeByAddress(this.state.address)
-    .then(results => getLatLng(results[0]))
-    .then(latLng => console.log("Success", latLng))
-    .catch(error => console.error("Error", error));
-  };
-
-  onChange = (address) => {
-    this.setState({ address });
-  };
-
-  handleScriptLoad = () => {
-    this.setState({
-      scriptLoaded: true
-    });
-  };
-
   render() {
-    const inputProps = {
-      value: this.state.address,
-      onChange: this.onChange
-    };
     const { incrementCounter, decrementCounter, data, openModal } = this.props;
 
     return (
       <div>
-        <Script
-          url={`https://maps.googleapis.com/maps/api/js?key=${config.googleApiKey}&libraries=places`}
-          onLoad={this.handleScriptLoad}
-        />
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color="green" content="Increment"/>
@@ -71,12 +33,6 @@ class TestComponent extends Component {
         <Button onClick={() => openModal("TestModal", { data: 43 })} color="teal" content="Open Modal"/>
         <br/>
         <br/>
-
-        <form onSubmit={this.handleFormSubmit}>
-          {this.state.scriptLoaded &&
-          <PlacesAutocomplete inputProps={inputProps}/>}
-          <button type="submit">Submit</button>
-        </form>
       </div>
     );
   }
