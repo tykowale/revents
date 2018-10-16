28b77c201d9201420f167b1cc48c8c29c366be69
tykowale <tykowale@gmail.com>
2018-10-15 19:44:21 -0500

    async reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -4,13 +4,15 @@ import testReducer from "../../features/testArea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
 import modalReducer from "../../features/modal/modalReducer";
 import authReducer from "../../features/auth/authReducer";
+import asyncReducer from "../../features/async/asyncReducer";
 
 const rootReducer = combineReducers({
   form: FormReducer,
   test: testReducer,
   events: eventReducer,
   modals: modalReducer,
-  auth: authReducer
+  auth: authReducer,
+  async: asyncReducer
 });
 
 export default rootReducer;
