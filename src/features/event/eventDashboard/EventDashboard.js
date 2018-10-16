b4c5b2ea0342247e6aa18ac10a862316d85da72c
tykowale <tykowale@gmail.com>
2018-10-15 21:01:11 -0500

    loading component

--- src/features/event/eventDashboard/EventDashboard.js
+++ src/features/event/eventDashboard/EventDashboard.js
@@ -3,35 +3,40 @@ import { connect } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import EventList from "../eventList/EventList";
 import { deleteEvent } from "../eventActions";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 const mapState = (state) => ({
-  events: state.events
+    events: state.events,
+    loading: state.async.loading
 });
 
 const actions = {
-  deleteEvent
+    deleteEvent
 };
 
 class EventDashboard extends Component {
 
-  handleDeleteEvent = (eventId) => () => {
-    this.props.deleteEvent(eventId);
-  };
+    handleDeleteEvent = (eventId) => () => {
+        this.props.deleteEvent(eventId);
+    };
 
-  render() {
-    const { events } = this.props;
+    render() {
+        const { events, loading } = this.props;
+        if (loading) {
+            return <LoadingComponent inverted={true}/>
+        }
 
-    return (
-      <Grid>
-        <Grid.Column width={10}>
-          <EventList deleteEvent={this.handleDeleteEvent} events={events}/>
-        </Grid.Column>
+        return (
+            <Grid>
+                <Grid.Column width={10}>
+                    <EventList deleteEvent={this.handleDeleteEvent} events={events}/>
+                </Grid.Column>
 
-        <Grid.Column width={6}>
-        </Grid.Column>
-      </Grid>
-    );
-  }
+                <Grid.Column width={6}>
+                </Grid.Column>
+            </Grid>
+        );
+    }
 }
 
 export default connect(mapState, actions)(EventDashboard);
