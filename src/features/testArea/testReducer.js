732f7d0eca497c1e730764618eb135f3d4166edf
tykowale <tykowale@gmail.com>
2018-10-15 19:37:00 -0500

    async test increment

--- src/features/testArea/testReducer.js
+++ src/features/testArea/testReducer.js
@@ -1,8 +1,9 @@
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
+import { INCREMENT_COUNTER, DECREMENT_COUNTER, COUNTER_ACTION_FINISHED, COUNTER_ACTION_STARTED } from "./testConstants";
 import { createReducer } from "../../app/common/util/reducerUtil";
 
 const initialState = {
-  data: 34
+  data: 34,
+  loading: false
 };
 
 export const incrementCounter = (state, payload) => {
@@ -13,7 +14,23 @@ export const decrementCounter = (state, payload) => {
   return { ...state, data: state.data - 1 };
 };
 
+export const counterActionStarted = (state, payload) => {
+  return {
+    ...state,
+    loading: true
+  };
+};
+
+export const counterActionFinished = (state, payload) => {
+  return {
+    ...state,
+    loading: false
+  };
+};
+
 export default createReducer(initialState, {
   [INCREMENT_COUNTER]: incrementCounter,
-  [DECREMENT_COUNTER]: decrementCounter
+  [DECREMENT_COUNTER]: decrementCounter,
+  [COUNTER_ACTION_STARTED]: counterActionStarted,
+  [COUNTER_ACTION_FINISHED]: counterActionFinished
 });
