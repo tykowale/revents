e79358345f5e50dcc9bbc67298ee21631a0dc718
tykowale <tykowale@gmail.com>
2018-10-16 19:57:11 -0500

    add notifications for create/update event

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { reducer as FormReducer } from "redux-form";
+import { reducer as toastrReducer } from "react-redux-toastr";
 import testReducer from "../../features/testArea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
 import modalReducer from "../../features/modal/modalReducer";
@@ -7,12 +8,13 @@ import authReducer from "../../features/auth/authReducer";
 import asyncReducer from "../../features/async/asyncReducer";
 
 const rootReducer = combineReducers({
-  form: FormReducer,
-  test: testReducer,
-  events: eventReducer,
-  modals: modalReducer,
-  auth: authReducer,
-  async: asyncReducer
+    form: FormReducer,
+    test: testReducer,
+    events: eventReducer,
+    modals: modalReducer,
+    auth: authReducer,
+    async: asyncReducer,
+    toastr: toastrReducer
 });
 
 export default rootReducer;
